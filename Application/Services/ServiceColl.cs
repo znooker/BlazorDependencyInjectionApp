d23d6fad784c0cc9b638ff375f0c7e8d30841fcf
Andreas <znooker_18@hotmail.com>
2024-02-02 18:01:20 +0100

    Service Collection added

--- Application/Services/ServiceColl.cs
+++ Application/Services/ServiceColl.cs
@@ -1,15 +1,14 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Application.Services
-{
-    public static class ServiceColl
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-        {
-            services.AddScoped<ITestService, TestService>();
+namespace Application.Services;
 
-            return services;
-        }
+public static class ServiceColl
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<ITestService, TestService>();
+
+        return services;
     }
 }
